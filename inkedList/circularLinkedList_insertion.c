38413164a0f52d5675ba14557e287845b3888248
Ashish <ashishsaud498@gmail.com>
2025-02-20 23:46:27 +0545

    insertion st begining for circular linked list

---
+++ inkedList/circularLinkedList_insertion.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+  int data;
+  struct Node *next;
+};
+
+void display(struct Node *head)
+{
+  if (head == NULL)
+  {
+    printf("List is empty\n");
+    return;
+  }
+  struct Node *current = head;
+  do
+  {
+    printf("%d -> ", current->data);
+    current = current->next;
+  } while (current != head);
+  printf("(head)\n");
+}
+
+void insertAtBeginning(struct Node **head, int data)
+{
+  struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+  newNode->data = data;
+  newNode->next = NULL;
+  if (*head == NULL)
+  {
+    *head = newNode;
+    newNode->next = *head;
+  }
+  else
+  {
+    struct Node *temp = *head;
+    while (temp->next != *head)
+    {
+      temp = temp->next;
+    }
+    temp->next = newNode;
+    newNode->next = *head;
+    *head = newNode;
+  }
+}
+
+int main()
+{
+  struct Node *head = NULL;
+
+  printf("\n After inserting at the beginning: \n");
+  insertAtBeginning(&head, 10);
+  insertAtBeginning(&head, 20);
+  insertAtBeginning(&head, 30);
+  display(head);
+}
