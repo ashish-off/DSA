b0473b308f65f8957701ee0c25608bc358b95d2e
Ashish <ashishsaud498@gmail.com>
2025-02-20 23:50:07 +0545

    insertion at end for circular linked list

--- inkedList/circularLinkedList_insertion.c
+++ inkedList/circularLinkedList_insertion.c
@@ -46,6 +46,24 @@ void insertAtBeginning(struct Node **head, int data)
   }
 }
 
+void insertAtEnd(struct Node** head, int data) {
+  struct Node* newNode = (struct Node *)malloc(sizeof(struct Node));
+  newNode->data = data;
+  newNode->next = NULL;
+
+  if (*head == NULL) {
+      *head = newNode;
+      newNode->next = *head; 
+  } else {
+      struct Node* last = *head;
+      while (last->next != *head) {
+          last = last->next; 
+      }
+      last->next = newNode;  
+      newNode->next = *head; 
+  }
+}
+
 int main()
 {
   struct Node *head = NULL;
@@ -55,4 +73,11 @@ int main()
   insertAtBeginning(&head, 20);
   insertAtBeginning(&head, 30);
   display(head);
+
+  printf("\n After inserting at the end: \n");
+  insertAtEnd(&head, 40);
+  insertAtEnd(&head, 50);
+  display(head);
+
+
 }
