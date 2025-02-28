d1b6ebe548fae6acfeb972827481a289ee91663a
Ashish <ashishsaud498@gmail.com>
2025-02-28 23:52:17 +0545

    stack implemented with pop function

--- stack/ArrayImplementation.c
+++ stack/ArrayImplementation.c
@@ -1,70 +1,110 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #define CAPACITY 100
 
 // strecture of stack
 
-struct Stack {
+struct Stack
+{
   int top;
   int items[CAPACITY];
 };
 
-void initializeStack(struct Stack *stack){
+void initializeStack(struct Stack *stack)
+{
   stack->top = -1;
 };
 
-int isFull (struct Stack *stack){
-  if (stack -> top == CAPACITY - 1 ){
+int isFull(struct Stack *stack)
+{
+  if (stack->top == CAPACITY - 1)
+  {
     return 1;
-  } else {
+  }
+  else
+  {
     return 0;
   }
 }
 
-int isEmpty (struct Stack *stack){
-  if (stack -> top == -1){
+int isEmpty(struct Stack *stack)
+{
+  if (stack->top == -1)
+  {
     return 1;
-  } else {
+  }
+  else
+  {
     return 0;
   }
 }
 
-int peek(struct Stack *stack){
-  if (isEmpty(stack)){
+int peek(struct Stack *stack)
+{
+  if (isEmpty(stack))
+  {
     printf("Stack is empty, UNDERFLOW \n");
     return -1;
-  } else {
-    return stack -> items[stack -> top];
+  }
+  else
+  {
+    return stack->items[stack->top];
   }
 }
 
-void display(struct Stack *stack){
-  if (isEmpty(stack)){
+void display(struct Stack *stack)
+{
+  if (isEmpty(stack))
+  {
     printf("\n Stack is empty, UNDERFLOW \n");
     return;
-  } else {
+  }
+  else
+  {
     printf("\n Stack elements are: ");
-    for (int i = 0; i <= stack -> top; i++){
-      printf("%d \t", stack -> items[i]);
+    for (int i = 0; i <= stack->top; i++)
+    {
+      printf("%d \t", stack->items[i]);
     }
     printf("\n");
   }
 }
 
-void push(struct Stack *stack, int data){
+void push(struct Stack *stack, int data)
+{
   if (isFull(stack))
   {
     printf("\n Stack is full, OVERFLOW \n");
     return;
-  } else {
-    stack -> top++;
-    stack -> items[stack -> top] = data;
   }
-  
+  else
+  {
+    stack->top++;
+    stack->items[stack->top] = data;
+    printf("\n %d is pushed \n", data);
+  }
 }
 
-int main (){
+void pop(struct Stack *stack)
+{
+  if (isEmpty(stack))
+  {
+    printf("\n Stack is empty, UNDERFLOW \n");
+    return;
+  
+  }else {
+    printf("\n %d is popped \n", stack->items[stack->top]);
+    stack->top--;
+  }
+}
+
+int main()
+{
   struct Stack stack;
   initializeStack(&stack);
+
+  push(&stack, 10);
+
+  display(&stack);
 }
