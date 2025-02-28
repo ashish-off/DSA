d7317e7830d7a14431f26c6a6598b5078ef2669b
Ashish <ashishsaud498@gmail.com>
2025-02-28 23:47:23 +0545

    stack implemented with push functions

--- stack/ArrayImplementation.c
+++ stack/ArrayImplementation.c
@@ -41,17 +41,30 @@ int peek(struct Stack *stack){
 
 void display(struct Stack *stack){
   if (isEmpty(stack)){
-    printf("Stack is empty, UNDERFLOW \n");
+    printf("\n Stack is empty, UNDERFLOW \n");
+    return;
   } else {
     printf("\n Stack elements are: ");
     for (int i = 0; i <= stack -> top; i++){
       printf("%d \t", stack -> items[i]);
     }
+    printf("\n");
   }
 }
 
+void push(struct Stack *stack, int data){
+  if (isFull(stack))
+  {
+    printf("\n Stack is full, OVERFLOW \n");
+    return;
+  } else {
+    stack -> top++;
+    stack -> items[stack -> top] = data;
+  }
+  
+}
 
 int main (){
   struct Stack stack;
-  initailizeStack(&stack);
+  initializeStack(&stack);
 }
