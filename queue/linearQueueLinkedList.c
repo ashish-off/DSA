1a37c4874707fc1a53bc7497ff4962f640abeea3
Ashish <ashishsaud498@gmail.com>
2025-03-06 16:44:48 +0545

    add dequeue function

--- queue/linearQueueLinkedList.c
+++ queue/linearQueueLinkedList.c
@@ -40,6 +40,28 @@ void enqueue(struct Queue* q, int value) {
   printf("Enqueued %d to the queue\n", value);
 }
 
+// Function to remove an element from the queue (Dequeue)
+int dequeue(struct Queue* q) {
+  if (isEmpty(q)) {
+      printf("Queue is empty! Cannot dequeue\n");
+      return -1;
+  } else {
+      // Remove the front node and update the front pointer
+      struct Node* temp = q->front;
+      int dequeuedValue = temp->data;
+      q->front = q->front->next;
+
+      // If the queue becomes empty after dequeue, update the rear pointer
+      if (q->front == NULL) {
+          q->rear = NULL;
+      }
+
+      free(temp); // Free the memory of the dequeued node
+      printf("Dequeued %d from the queue\n", dequeuedValue);
+      return dequeuedValue;
+  }
+}
+
 
 int main (){
 
