df4559978d2949edf157f11f2a8d061a56ff9919
Ashish <ashishsaud498@gmail.com>
2025-03-10 02:31:00 +0545

    bubble sort improvement

--- sorting/bubblesort.c
+++ sorting/bubblesort.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-
+//
 void bubbleSort(int arr[], int n)
 {
   for (int i = 0; i < n - 1; i++) // acess arrary element
   {
-    for (int j = 0; j < n - 1 - i; j++) // to compare the adjacent elements
+    int swap = 0;
+    for (int j = 0; j < n - 1 - i; j++) // to compare the adjacent elements (one less each time)
     {
       if (arr[j] > arr[j + 1])
       {
@@ -12,7 +13,12 @@ void bubbleSort(int arr[], int n)
         int temp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
+        swap = 1;
       }
+    }
+    if (swap == 0)
+    {
+      break;
     }
   }
 }
