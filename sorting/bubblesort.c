9e7ddf9b939bbbefc4f5826fe4bec265d3b4f4ac
Ashish <ashishsaud498@gmail.com>
2025-03-09 23:58:06 +0545

    add bubble sort

--- sorting/bubblesort.c
+++ sorting/bubblesort.c
@@ -23,6 +23,26 @@ void bubbleSort(int arr[], int n)
   }
 }
 
+void printArray(int arr[], int size)
+{
+  for (int i = 0; i < size; i++)
+  {
+    printf("%d ", arr[i]);
+  }
+  printf("\n");
+}
+
 int main()
 {
+  int arr[] = {64, 34, 25, 12, 22, 11, 90};
+  int n = sizeof(arr) / sizeof(arr[0]);
+
+  printf("Original array: \n");
+  printArray(arr, n);
+
+  bubbleSort(arr, n);
+
+  printf("Sorted array: \n");
+  printArray(arr, n);
+  return 0;
 }
