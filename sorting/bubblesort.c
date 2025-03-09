6436b15df16a203bc2f8d1458170fa4c8375abcb
Ashish <ashishsaud498@gmail.com>
2025-03-10 01:55:47 +0545

    bubble sort implementation

--- sorting/bubblesort.c
+++ sorting/bubblesort.c
@@ -2,23 +2,17 @@
 
 void bubbleSort(int arr[], int n)
 {
-  for (int i = 0; i < n - 1; i++)
+  for (int i = 0; i < n - 1; i++) // acess arrary element
   {
-    int swapped = 0;
-    for (int j = 0; j < n - i - 1; j++)
+    for (int j = 0; j < n - 1 - i; j++) // to compare the adjacent elements
     {
       if (arr[j] > arr[j + 1])
       {
-        // Swap arr[j] and arr[j + 1]
+        // swap if the elements are not in ascending order
         int temp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
-        swapped = 1;
       }
-    }
-    if (swapped == 0)
-    {
-      break;
     }
   }
 }
@@ -34,7 +28,7 @@ void printArray(int arr[], int size)
 
 int main()
 {
-  int arr[] = {64, 34, 25, 12, 22, 11, 90};
+  int arr[] = {99, 67, 54, 2, 44, 1, 5};
   int n = sizeof(arr) / sizeof(arr[0]);
 
   printf("Original array: \n");
