3b0cde1bb4d94667a03db0363b56b60aefe997e4
Ashish <ashishsaud498@gmail.com>
2025-03-11 01:50:26 +0545

    inertion sort implementation

---
+++ sorting/insertionSort.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void insertionSort(int arr[], int n)
+{
+  for (int i = 1; i < n; i++)
+  {
+    int key = arr[i];
+    int j = i - 1;
+
+    while (j >= 0 && arr[j] < arr[key])
+    {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = key;
+  }
+}
+
+void printArray(int arr[], int n)
+{
+  for (int i = 0; i < n - 1; i++)
+  {
+    printf("%d ", arr[i]);
+  }
+  printf("\n");
+}
+
+
+int main()
+{
+
+}
